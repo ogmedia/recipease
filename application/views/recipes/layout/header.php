be2282a174578b0102b362b36d258c9b721f0334
Jason Martinez <jason@arcanalabs.com>
2012-05-20 16:58:55 -0700

    Transitions fix for the flickering badness.

--- application/views/recipes/layout/header.php
+++ application/views/recipes/layout/header.php
@@ -7,5 +7,12 @@
   <link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.css" />
 	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.js"></script>
+
+	<style>
+		.ui-page {
+		-webkit-backface-visibility: hidden;
+		}
+	</style>
+
  </head>
  <body>
