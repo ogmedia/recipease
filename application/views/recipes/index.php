540a380e39509e3705670e60b33a9b9b99a73e69
Lars Ganser <sean@organgrindermedia.com>
2012-05-20 14:03:54 -0700

    slight changes to library and changed time

--- application/views/recipes/index.php
+++ application/views/recipes/index.php
@@ -2,6 +2,6 @@
 <br />
 <ul id="recipe_list">
 <?foreach($recipes as $recipe){?>
-	<li id="<?=$recipe['id']?>"><a href="/recipes/view/<?=$recipe['id']?>"><?=$recipe['title']?></a> (<?=date( 'Y-m-d',strtotime($recipe['created']) );?>)<?=$recipe['id']?></li>
+	<li id="<?=$recipe['id']?>"> <?=$recipe['id']?>: &nbsp;<a href="/recipes/view/<?=$recipe['id']?>"><?=$recipe['title']?></a> (<?=date( 'M j, Y g:ia',strtotime($recipe['created']) );?>)</li>
 <?}?>
 </ul>
