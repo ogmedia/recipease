38330bcdd75e953f5c6538fba443db79b2006362
Lars Ganser <sean@organgrindermedia.com>
2012-05-20 21:58:24 -0700

    updated history page

--- application/views/recipes/recipe/history.php
+++ application/views/recipes/recipe/history.php
@@ -1,32 +1,30 @@
 <div>
-<h2>Recipe History</h2>
-<ul>
-	<? foreach( $recipe_history as $recipe ){ ?>
-	<li id="recipe_<?=$recipe['data']['id']?>" class="recipe_edit">
-	<div class="recipe_title"><?=$recipe['data']['title'];?></div>
-	<div class="created_on"><span class="created_date">(<?=date( 'M j, Y g:ia',strtotime($recipe['data']['created']) );?>)</span> |
-		<?if( empty( $recipe['data']['parent_id'] ) ){?>
-	 		Original Version
-		<? }else{ ?>
-	 		<span class="view_changes"><a href="javascript:;">View Changes</a></span>
-	 	<? } ?>
-	</div>
-	<div class="recipe_changes" style="display:none;">
-		<h3>RECIPE CHANGES</h3>
-		<? foreach($recipe['changes'] as $changed_member => $changes){?>
-			<div class="changed_details">
-				<h4><?=$changed_member?></h4>
-				<? foreach($changes as $change){?>
-					<? foreach( $change as $field => $value ){ ?>
-					<span>(<?=$field;?>: <?=$value?>)</span> 
-					<? } ?>
+	<h2>Recipe History</h2>
+	<div>
+		<? foreach( $recipe_history as $recipe ){ ?>
+		<div id="recipe_<?=$recipe['data']['id']?>" class="recipe_edit">
+			<div class="recipe_changes" style="">
+				<h3><?=date( 'M j, Y g:ia',strtotime($recipe['data']['created']) );?></h3>
+				<div class="created_on">
+					<span class="created_date"></span>
+					<?if( empty( $recipe['data']['parent_id'] ) ){?>
+				 		| Original Version
+					<? }?>
+				</div>
+				<? foreach($recipe['changes'] as $changed_member => $changes){?>
+					<div class="changed_details">
+						<h4><?=$changed_member?></h4>
+						<? foreach($changes as $change){?>
+							<? foreach( $change as $field => $value ){ ?>
+							<span><?=$field;?>: <?=$value?></span>,
+							<? } ?>
+						<? } ?>
+					</div>
 				<? } ?>
 			</div>
+		</div>
 		<? } ?>
 	</div>
-	</li>
-	<? } ?>
-</ul>
 </div>
 
 <script type="text/javascript">
@@ -40,6 +38,7 @@
 .recipe_edit{
 	float:left;
 	clear:left;
+	margin:10px;
 }
 .recipe_title{
 	width:400px;
